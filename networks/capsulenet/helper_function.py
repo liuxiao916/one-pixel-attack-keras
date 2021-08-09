7dde4aa3fbeab611787abf5b09f5c1f1d4d7b71b
liuxiao <971177213@qq.com>
2021-08-09 16:55:37 +0800

    tf 2.5

--- networks/capsulenet/helper_function.py
+++ networks/capsulenet/helper_function.py
@@ -1,7 +1,7 @@
 import os, math, csv
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.utils import to_categorical
+from tensorflow.python.keras.utils.np_utils import to_categorical
 from pandas import read_csv
 
 def load_cifar_10():
